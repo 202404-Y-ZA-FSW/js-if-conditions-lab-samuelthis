fd76b54c65cd07a60466be1c3ed2298bf09613f2
samuel <31856641+samuelthis@users.noreply.github.com>
2024-05-02 11:30:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Write your code here
+function challenge (number)
+{
+  if (number >10 && number <20) 
+  {
+    console.log(number)
+  } else if (number >20)
+    console.log(number*number);
+}
 
+else if (number <100) {
+  return number/2;
+}
+let answer = challenge (96);
 // After your finish you can run the following command to start the server
 // node index.js
